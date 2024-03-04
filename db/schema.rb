690b8369c71e3bd8f180b78b15ce52859507d200
takahiro0326 <161424302+takahiro0326@users.noreply.github.com>
2024-03-04 17:23:51 +0900

    ユーザー認証機能の実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_04_062031) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_04_081826) do
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", default: "", null: false
